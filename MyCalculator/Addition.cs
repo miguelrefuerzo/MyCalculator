a9c5895d9019d955b2d7be5b6efc8ee9d16e7021
Miguel Refuerzo <miguel.refuerzo@softvision.com>
2022-06-08 15:47:46 +0800

    1. Added a class for addition function 2. Added a switch case

---
+++ MyCalculator/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    internal static class Addition
+    {
+        public static void Additions(int x, int y) 
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+    }
+}
--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -1,4 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a calculator console app system");
-Console.WriteLine("Hello there would you like to add?");
+using MyCalculator;
+char action;
+int x, y;
+Console.WriteLine("What would you like to do? \na. Add\nb. Subtract\nc. Multiply\nd. Divide");
+
+action = Console.ReadLine()[0];
+
+
+switch (action)
+{
+    case 'a':
+        Console.WriteLine("Addition");
+        Console.WriteLine("Number:");
+        x = Convert.ToInt32(Console.ReadLine());
+        y = Convert.ToInt32(Console.ReadLine());
+        Addition.Additions(x, y);
+        break;
+    default:
+        break;
+}
